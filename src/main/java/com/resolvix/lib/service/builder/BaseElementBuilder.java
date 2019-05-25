c91e72520cf0d4d4a16df8a274b2fc5dacb2df00
Roger Bisson <rwbisson@resolvix.com>
2019-05-26 00:53:48 +0100

    Added BaseElementBuilder abstract class

---
+++ src/main/java/com/resolvix/lib/service/builder/BaseElementBuilder.java
@@ -0,0 +1,34 @@
+package com.resolvix.lib.service.builder;
+
+/**
+ * A base implementation of an element builder class for a root element, or
+ * a subordinate element.
+ *
+ * If the builder is for a root element, the element builder subclass should
+ * return an object of the root element type. If the element builder is for a
+ * subordinate element, the subclass should return an object of the
+ * superordinate element builder.
+ *
+ * @param <S> the element builder subclass type
+ * @param <R> the return type
+ */
+public abstract class BaseElementBuilder<S extends BaseElementBuilder<S, R>, R> {
+
+    public BaseElementBuilder() {
+        //
+    }
+
+    /**
+     * Returns the element builder instance.
+     *
+     * @return the builder object
+     */
+    public abstract S self();
+
+    /**
+     * Build the element and returns the return object.
+     *
+     * @return the return object
+     */
+    public abstract R build();
+}
