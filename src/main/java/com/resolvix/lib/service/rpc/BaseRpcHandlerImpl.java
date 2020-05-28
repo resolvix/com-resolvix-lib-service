251b42b20af61a6fe2ce17c63ecaec603473caab
Roger Bisson <rwbisson@resolvix.com>
2020-05-28 20:22:22 +0100

    Added {RPC,SOAP,WS} handlers / message handlers

---
+++ src/main/java/com/resolvix/lib/service/rpc/BaseRpcHandlerImpl.java
@@ -0,0 +1,48 @@
+package com.resolvix.lib.service.rpc;
+
+import org.slf4j.Logger;
+
+import javax.xml.namespace.QName;
+import javax.xml.rpc.handler.Handler;
+import javax.xml.rpc.handler.HandlerInfo;
+import javax.xml.rpc.handler.MessageContext;
+
+public abstract class BaseRpcHandlerImpl
+    implements Handler
+{
+    protected abstract Logger getLogger();
+
+    @Override
+    public void init(HandlerInfo handlerInfo) {
+        getLogger().debug("BaseRpcHandlerImpl::init invoked.");
+    }
+
+    @Override
+    public QName[] getHeaders() {
+        getLogger().debug("BaseRpcHandlerImpl::getHeaders invoked.");
+        return new QName[0];
+    }
+
+    @Override
+    public boolean handleRequest(MessageContext messageContext) {
+        getLogger().debug("BaseRpcHandlerImpl::handleRequest invoked.");
+        return true;
+    }
+
+    @Override
+    public boolean handleResponse(MessageContext messageContext) {
+        getLogger().debug("BaseRpcHandlerImpl::handleResponse invoked.");
+        return true;
+    }
+
+    @Override
+    public boolean handleFault(MessageContext messageContext) {
+        getLogger().debug("BaseRpcHandlerImpl::handleFault invoked.");
+        return true;
+    }
+
+    @Override
+    public void destroy() {
+        getLogger().debug("BaseRpcHandlerImpl::destroy invoked.");
+    }
+}
